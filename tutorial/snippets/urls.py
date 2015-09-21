7c0ccdb7f8f972584146f84b91e8c458132f9538
psuarez <hostsuarezpablo@gmail.com>
2015-09-21 18:43:45 -0300

    update

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
-from django.conf.urls import include
 
-urlpatterns = [
-
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
+# API endpoints
+urlpatterns = format_suffix_patterns([
     url(r'^$', views.api_root),
     url(r'^snippets/$',
         views.SnippetList.as_view(),
@@ -22,4 +20,10 @@ urlpatterns = [
     url(r'^users/(?P<pk>[0-9]+)/$',
         views.UserDetail.as_view(),
         name='user-detail')
+])
+
+# Login and logout views for the browsable API
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
