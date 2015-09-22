02da9c51774fb9f82d5f39eaed9444ff04d602e0
psuarez <hostsuarezpablo@gmail.com>
2015-09-22 14:05:00 -0300

    using rest_framework.routers

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -10,9 +10,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
-
 from rest_framework import viewsets
-
 from rest_framework.decorators import detail_route
 
 
